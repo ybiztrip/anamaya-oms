bb4b63d102be5cfae2f5b65cc48071112ceaf734
sabriyanagungwibowo <sabriyanagung@gmail.com>
2025-11-19 22:25:13 +0700

    publish message

--- core/src/main/java/ai/anamaya/service/oms/core/client/queue/BookingPubSubPublisher.java
+++ core/src/main/java/ai/anamaya/service/oms/core/client/queue/BookingPubSubPublisher.java
@@ -2,28 +2,34 @@ package ai.anamaya.service.oms.core.client.queue;
 
 import ai.anamaya.service.oms.core.dto.pubsub.BookingStatusMessage;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.google.protobuf.ByteString;
+import com.google.pubsub.v1.PubsubMessage;
+import org.springframework.stereotype.Component;
 import com.google.cloud.spring.pubsub.core.PubSubTemplate;
-import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
-import org.springframework.stereotype.Service;
 
-@Service
-@RequiredArgsConstructor
-@Slf4j
+@Component
 public class BookingPubSubPublisher {
-    private final PubSubTemplate pubSubTemplate;
-    private final ObjectMapper mapper;
 
-    private final String topicId = "oms-booking-status";
+    private final PubSubTemplate pubSubTemplate;
+
+    public BookingPubSubPublisher(PubSubTemplate pubSubTemplate) {
+        this.pubSubTemplate = pubSubTemplate;
+    }
+
+    private final ObjectMapper objectMapper = new ObjectMapper();
 
     public void publishBookingStatus(BookingStatusMessage message) {
         try {
-            String json = mapper.writeValueAsString(message);
-            pubSubTemplate.publish(topicId, json);
-            log.info("Published booking status message: {}", json);
+            String json = objectMapper.writeValueAsString(message);
+            ByteString data = ByteString.copyFromUtf8(json);
+            PubsubMessage pubsubMessage = PubsubMessage.newBuilder()
+                .setData(data)
+                .build();
+
+            pubSubTemplate.publish("oms-booking-status", pubsubMessage);
+
         } catch (Exception e) {
-            log.error("Failed to publish booking status", e);
+            throw new RuntimeException("Failed to publish pubsub message", e);
         }
     }
 }
-
